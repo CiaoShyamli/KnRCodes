dcb727f9be69090c79ab9522eea241bcb179ed84
Shyamli Arora <shyamliarora6@gmail.com>
2017-03-01 07:57:28 -0600

    Add files via upload

---
+++ 2.1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("%d %d %d %d ",sizeof(int),sizeof(char),sizeof(float),sizeof(double));
+	getch();
+}
---
+++ 2.2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int i=0;
+	int limit=100;
+	while(i<lim-1)/*for(i=0;i<lim-1&&(c=getchar())!='\n'&&c!=EOF;i++)*/
+	{
+		c=getchar();
+		if(c==EOF)
+		{
+			break;
+		}
+		elseif(c=='\n')
+		{
+			break;
+		}
+		i++;
+	}
+	getch();
+}
