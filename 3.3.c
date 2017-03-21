9ac65990e0478dd27a5ad8f043e1c76029b39a93
Shyamli Arora <shyamliarora6@gmail.com>
2017-03-21 08:51:01 -0500

    Add files via upload

--- 3.3.c
+++ 3.3.c
@@ -1,40 +1,44 @@
 #include<stdio.h>
 #include<conio.h>
 void expand();
-char s[200],s2[200];
+char s[200], s2[200];
 void main()
 {
- clrscr();
- printf(" enter  your string\n=");
- gets(s);
- expand();
- printf("\n printing expanded string");
- puts(s2);
- getch();
+
+	printf("Enter your string : ");
+	gets(s);
+	expand();
+	printf("\nPrinting expanded string : \n\n");
+	puts(s2);
+	printf("\n");
+	printf("************************* Entered String Expanded *************************");
+	_getch();
 }
 
 void expand()
 {
-int iterator,iterator2=0;
-char temp,temp2,ch;
- for(iterator=0;s[iterator]!='\0';iterator++)
- {
-    if(s[iterator]=='0'||s[iterator]=='a')
-    {
-      temp=s[iterator];
-      if(s[++iterator]=='-')
+	int iterator, iterator2 = 0;
+	char temp, temp2, ch;
+	for (iterator = 0; s[iterator] != '\0'; iterator++)
 	{
-	  if(s[++iterator]=='9'||s[++iterator]=='z')
-	  {
-	    temp2=s[iterator];
-	   for(ch=temp;ch<=temp2;ch++)
-	   {
-		s2[iterator2++]=ch;
-	   }
-	  }
+		if (s[iterator] == '0' || s[iterator] == 'a')
+		{
+			temp = s[iterator];
+			if (s[++iterator] == '-')
+			{ 
+				iterator++;
+				if (s[iterator] == '9' || s[iterator] == 'z')
+				{
+					temp2 = s[iterator];
+				//	printf("%c  %c", temp, temp2);
+					for (ch = temp; ch <= temp2; ch++)
+					{
+						s2[iterator2++] = ch;
+					}
+				}
+			}
+		}
 	}
-    }
- }
- s2[iterator2]='\0';
+	s2[iterator2] = '\0';
 }
 
