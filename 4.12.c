ca4b2af30615c464aca81410a590726d2273b99e
Shyamli Arora <shyamliarora6@gmail.com>
2017-08-16 11:29:28 -0500

    Add files via upload

---
+++ 4.12.c
@@ -0,0 +1,55 @@
+/*Adapt the ideas of printd to write a recursive version of itoa; that is,
+convert an integer into a string by calling a recursive routine.*/
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void itoa(int);
+void reverse( char []);
+char s[100];
+void main()
+{
+	int c,i=0;
+	printf(" Enter integer");
+	scanf("%d",&c);
+	itoa(c);  /* itoa:  convert n to characters in s */
+	while(s[i++]!='\0')
+	{
+  printf("%c",s[i]);
+	}
+	_getch();
+}
+
+
+ void itoa(int n)   
+  {      
+  int i, sign;
+ if((sign = n) < 0)  /* record sign */ 
+ n = -n;          /* make n positive */ 
+ i = 0; 
+   if((n /= 10) > 0)
+   {/* generate digits in reverse order */ 
+ itoa(s[i++] = n % 10 + '0');  /* get next digit */   
+ }
+   else
+   {
+	   return ;
+   }
+ if(sign < 0) 
+ s[i++] = '-'; 
+ s[i] = '\0';  
+ reverse(s);  
+ }
+
+  
+   /* reverse:  reverse string s in place */  
+   void reverse(char s[])  
+   {      
+   int c, i, j;
+       for (i = 0, j = strlen(s)-1; i < j; i++, j--) 
+	   {  
+	   c = s[i];    
+       s[i] = s[j];    
+       s[j] = c;   
+	   } 
+   } 
+	   
